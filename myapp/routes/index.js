540201e77cf7dca497e46f8dec57b41003c59609
shun-g <1901015033hh@cyber-u.ac.jp>
2023-11-25 14:45:04 +0900

    print json

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+const request = require('request');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-
+router.get('/', async (req, res) => {
+  request('https://dog.ceo/api/breeds/image/random', function (error, response,body) {
+    if (!error && response.statusCode == 200) {
+      const data = JSON.parse(body);
+      res.json(data);
+    }
+  });
+})
 module.exports = router;
